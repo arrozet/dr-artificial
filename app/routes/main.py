e85996a156f1c18b31a34b826987b53d404bc9e5
Juan Manuel Valenzuela González <amcgil@uma.es>
2025-03-12 12:18:01 +0100

    VAN LOS ENLACES
    davi he cambiado para que funcione con elk mio cuando lo vayas a usar cambialo por index.html y yasta

--- app/routes/main.py
+++ app/routes/main.py
@@ -2,9 +2,10 @@ from flask import Blueprint, render_template, request
 
 main_bp = Blueprint("main", __name__)
 
+#PARA QUE FUNCIONE EN INTERFAZ1 O INDEX CAMBIAR EN DONDE HE COMENTADO
 @main_bp.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("interfaz1.html") # AQUI
 
 
 @main_bp.route('/', methods=['POST'])
@@ -12,7 +13,7 @@ def procesarPrompt():
     prompt = request.form['prompt']  # Elimina espacios en blanco
 
     mi_mensaje = f"""
-        <div class="message">
+        <div class="UserMessage">
             <div class="avatar user">U</div>
             <div class="message-content">
                 <div class="message-bubble">
@@ -21,10 +22,10 @@ def procesarPrompt():
             </div>
         </div>
         """
-    
+
     respuesta = "HOLA"
     respuesta_html = f"""
-        <div class="message">
+        <div class="AiMessage">
             <div class="avatar">AI</div>
             <div class="message-content">
                 <div class="message-bubble">
@@ -35,6 +36,10 @@ def procesarPrompt():
         """
 
     if not prompt:  # Validación para evitar enviar texto vacío
-        return render_template('index.html', mensaje='No puedes enviar un texto vacío.')
+        return render_template('interfaz1.html', mensaje='No puedes enviar un texto vacío.') #AQUI
 
-    return render_template('index.html', mi_mensaje=mi_mensaje, respuesta=respuesta_html)
+    return render_template('interfaz1.html', mi_mensaje=mi_mensaje, respuesta=respuesta_html) #AQUI
+
+@main_bp.route('/<chat_name>')
+def mostrar_chat(chat_name):
+    return render_template(f'{chat_name}.html')
